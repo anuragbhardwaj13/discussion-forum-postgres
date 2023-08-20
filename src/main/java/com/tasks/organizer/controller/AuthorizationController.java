9217a8f598f4498e94b99aac80db5887e7c857a2
Marcos Sousa <marcosviniciocardoso123@gmail.com>
2023-08-19 21:08:15 -0300

    initial commit

---
+++ src/main/java/com/tasks/organizer/controller/AuthorizationController.java
@@ -0,0 +1,30 @@
+package com.tasks.organizer.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/resource")
+@RequiredArgsConstructor
+public class AuthorizationController {
+
+    @GetMapping
+    public ResponseEntity<String> sayHello() {
+        return ResponseEntity.ok("Here is your resource");
+    }
+
+    @GetMapping("/user")
+    public ResponseEntity<String> user() {
+        return ResponseEntity.ok("Here is your resource user");
+    }
+
+    @GetMapping("/admin")
+    public ResponseEntity<String> admin() {
+        return ResponseEntity.ok("Here is your resource admin");
+    }
+
+}
